2c3e0dcabde80666aaa5426f0f4e14b1d4753633
maxamx <maxamx@gmail.com>
2021-09-02 11:17:43 -0300

    Initial commit

---
+++ src/test/java/maxamx/estudo/NativeInicioResourceIT.java
@@ -0,0 +1,9 @@
+package maxamx.estudo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeInicioResourceIT extends InicioResourceTest {
+
+    // Execute the same tests but in native mode.
+}
